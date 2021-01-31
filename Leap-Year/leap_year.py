adeed15b88982b37b2e149d2f7db15bcd784513c
Abhi Balijepalli <AbhiBalijepalli@gmail.com>
2021-01-30 16:08:25 -0800

    leap year with error handling

--- Leap-Year/leap_year.py
+++ Leap-Year/leap_year.py
@@ -2,10 +2,13 @@
 # Date: 1/30/2020
 # Description: Check if a year given is leap year
 
-# To run the file: python3 leap_year_without_error_handling.py
+# To run the file: python3 python3 leap_year.py
 
 def leapyear():
     year = input("Please enter a year, and i will tell you if its a leap year or not: ")
+    if int(year) < 0 or int(year) is None:
+        print("Invalid Input \n")
+        return leapyear()
     if int(year) % 4 == 0 and int(year) % 100 != 0:
         print (year, "is a leap year")
         return
@@ -15,5 +18,6 @@ def leapyear():
     else:
         print(year, "is not a leap year")
         return
+ 
 if __name__ == "__main__":
     leapyear()
